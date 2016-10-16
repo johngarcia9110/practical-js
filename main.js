f25078496db0159cbc7aeef391d76b5bc44626aa
John B Garcia <johngarcia9110@gmail.com>
2016-10-16 19:12:15 -0400

    version 3 of todo list

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("JS loaded");
+
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3', 'item 4'],
+  displayTodos: function(){
+    console.log(this.todos);
+  },
+  addTodo: function(todo){
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue){
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(position,1);
+    this.displayTodos();
+  }
+}
