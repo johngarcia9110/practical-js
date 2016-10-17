0b9f5d48b945e18a0d63383106cc3ff97cc08a1e
John B Garcia <johngarcia9110@gmail.com>
2016-10-16 20:11:44 -0400

    added logic for displaying more information about each todo. Completed version 5

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ console.log("JS loaded");
 var todoList = {
   todos: [],
   displayTodos: function(){
-    console.log(this.todos);
+    if(this.todos.length === 0){
+      console.log('You have no todo\'s in your list!');
+    }else{
+      console.log('My Todos: ');
+      for(var i = 0; i < this.todos.length; i++){
+        if(this.todos[i].completed === true){
+          console.log( '(x)', this.todos[i].todoText);
+        }else{
+          console.log( '( )', this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText){
     this.todos.push({
